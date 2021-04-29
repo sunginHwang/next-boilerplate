f15cc33aa83d54c4fce9cdf9f0939fcdb44dfafe
sunginHwang <gommpo111@gmail.com>
2021-04-29 23:26:35 +0900

    eslint(airbnb룰) + prettier 적용

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // singleQuote formatting
+  singleQuote: true,
+  //코드 마지막 세미콜른
+  semi: true,
+  //탭이 아닌 space 사용
+  useTabs: false,
+  // 들여쓰기 너비는 2칸
+  tabWidth: 2,
+  trailingComma: 'all',
+  // codeLine maximum 120칸
+  printWidth: 120,
+  // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략
+  arrowParens: 'avoid',
+};
