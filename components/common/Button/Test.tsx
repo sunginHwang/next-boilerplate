987f32d40adc11e94a38d8e97c019cdb648753a2
sunginHwang <gommpo111@gmail.com>
2021-05-03 15:30:52 +0900

    commit terminal test

--- components/common/Button/Test.tsx
+++ components/common/Button/Test.tsx
@@ -1,5 +1,5 @@
 function Test() {
-  return <div>허스12121키, lint-staged 테스트333용 컴포넌트 입니다.1234</div>;
+  return <div>허스12121키, lint-32424343staged 테스트34343433용 컴포넌트 입니다.1234</div>;
 }
 
 export default Test;
